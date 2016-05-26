17e669a234825d0195cafe1c1bee34454a56c27e
edmundo <edmundo@SuperBacteria>
2016-05-26 15:19:28 -0600

    Este es mi avance. Hector

--- caca/src/caca/Caca.java
+++ caca/src/caca/Caca.java
@@ -3,20 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tutoguit2;
+package caca;
 
 /**
  *
  * @author edmundo
  */
-public class TutoGuit2 {
+public class Caca {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
     }
     
 }
